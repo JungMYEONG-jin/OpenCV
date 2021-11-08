632fa97827d2642d25def760cca29d2e566ac2c1
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2021-11-08 23:23:14 +0900

    video 녹화 기능

---
+++ video/videotake.cpp
@@ -0,0 +1,57 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+
+#define CV_8U 0
+#define CV_8S 1
+#define CV_16U 2
+#define CV_16S 3
+#define CV_32S 4	//int
+#define CV_32F 5	//float 
+#define CV_64F 6	// double
+#define CV_16F 7	//float16_t
+
+
+// mat class 는 여러 채널을 갖는다. 각 채널은 자료형을 지정할수 있음. but 하나의 채널에는 통일된 자료형을 사용해야함!
+// gray scale 1 channel
+// true color 3 channel
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	cout << "HELLO CV " << CV_VERSION << endl;
+	
+	VideoCapture cap;
+	cap.open("stopwatch.avi");
+	if (!cap.isOpened())
+	{
+		cout << "Fail\n";
+		return 0;
+	}
+
+	double fps = cap.get(CAP_PROP_FPS);
+	cout << "FPS: " << fps << endl;
+
+	int delay = cvRound(1000 / fps);
+	Mat frame;
+	Mat inv;
+	while (true)
+	{
+		cap >> frame;
+
+		if (frame.empty())
+			break;
+		inv = ~frame;
+		imshow("frame", frame);
+		imshow("inversed", inv);
+		if (waitKey(delay) == 27) // 낮을수록 빠름
+			break;
+	}
+
+	return 0;
+}
+
+
