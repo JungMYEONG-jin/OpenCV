db5d7bbe1a10812f71f23418952cfe1bf36643c4
MJ1996 <55305549+JungMYEONG-jin@users.noreply.github.com>
2021-11-08 23:40:02 +0900

    동영상에 선 나타내기

---
+++ video/동영상그림.cpp
@@ -0,0 +1,61 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+
+#define CV_8U 0
+#define CV_8S 1
+#define CV_16U 2
+#define CV_16S 3
+#define CV_32S 4	//int
+#define CV_32F 5	//float 
+#define CV_64F 6	// double
+#define CV_16F 7	//float16_t
+
+
+// mat class 는 여러 채널을 갖는다. 각 채널은 자료형을 지정할수 있음. but 하나의 채널에는 통일된 자료형을 사용해야함!
+// gray scale 1 channel
+// true color 3 channel
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	cout << "HELLO CV " << CV_VERSION << endl;
+	
+	Mat img(400, 400, CV_8UC3, Scalar(255, 255, 255));
+	line(img, Point(50, 50), Point(200, 50), Scalar(0, 0, 255)); // 직선그리기
+	line(img, Point(50, 100), Point(200, 100), Scalar(255, 0, 255), 3);
+	line(img, Point(50, 150), Point(200, 150), Scalar(255, 0, 0), 10);
+
+	line(img, Point(250, 50), Point(350, 100), Scalar(0, 0, 255),1,LINE_4); // 직선그리기
+	line(img, Point(250, 70), Point(350, 120), Scalar(255, 0, 255), 1, LINE_8);
+	line(img, Point(250, 90), Point(350, 140), Scalar(255, 0, 0), 1, LINE_AA);
+
+	arrowedLine(img, Point(50, 200), Point(150, 200), Scalar(0, 0, 255), 1);
+	arrowedLine(img, Point(50, 250), Point(350, 250), Scalar(255, 0, 255), 1);
+	arrowedLine(img, Point(50, 300), Point(350, 300), Scalar(255, 0, 0), 1, LINE_8, 0, 0.05);
+
+
+	drawMarker(img, Point(50, 350), Scalar(0, 0, 255), MARKER_CROSS);
+	drawMarker(img, Point(100, 350), Scalar(0, 0, 255), MARKER_TILTED_CROSS);
+	drawMarker(img, Point(150, 350), Scalar(0, 0, 255), MARKER_STAR);
+	drawMarker(img, Point(200, 350), Scalar(0, 0, 255), MARKER_DIAMOND);
+	drawMarker(img, Point(250, 350), Scalar(0, 0, 255), MARKER_SQUARE);
+	drawMarker(img, Point(300, 350), Scalar(0, 0, 255), MARKER_TRIANGLE_UP);
+	drawMarker(img, Point(350, 350), Scalar(0, 0, 255), MARKER_TRIANGLE_DOWN);
+	
+
+
+
+
+	imshow("img", img);
+	waitKey();
+	destroyAllWindows();
+
+
+	return 0;
+}
+
+
